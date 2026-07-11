a2d6b7646d1da81ca8d6dd37f9afa94db301ff5e
Muhammad Ali <72260305+ali-dev178@users.noreply.github.com>
2026-07-11 13:45:59 +0200

    claudometer 1.1.6

--- Casks/claudometer.rb
+++ Casks/claudometer.rb
@@ -2,8 +2,8 @@
 # after a release. Publish via your own tap repo (homebrew-claudometer), then:
 #   brew install --cask ali-dev178/claudometer/claudometer
 cask "claudometer" do
-  version "1.1.2"
-  sha256 "3985a278316da349e07e6b9b6ffb227d4f21599a2616a0af1d4e3048793c0611"
+  version "1.1.6"
+  sha256 "142e9194250d62fff232472adc24da30a70188a8727d5595e4a71e04ae35607e"
 
   url "https://github.com/ali-dev178/claudometer/releases/download/v#{version}/Claudometer-macos.zip"
   name "Claudometer"
